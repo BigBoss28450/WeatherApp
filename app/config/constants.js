12dd072a8025b85f8f5d6ffeca2ec7a702f3c8d0
Alexis Isidoro Bolaños Avalos <alexis.isidoro_91@hotmail.com>
2020-12-05 17:05:45 -0600

    Se coloco la api de weather maps en un archivo ENV el cual se añadio al gitignore para posteriormente almacenar el proyecto en git hub sin compromete la llave de la api

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,7 @@
+import { WEATHER_API_KEY } from './ENV';
+
 const API_URL = 'https://api.openweathermap.org/';
-const API_KEY = '5e22cdf392ec083c2aceed6fecc09977';
+const API_KEY = WEATHER_API_KEY;
 const CITY_LIST = ['Colima', 'Guadalajara', 'Sinaloa', 'Nuevo Leon'];
 
 export default {
